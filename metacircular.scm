dbfbfae56836cb8846573bc2ab91427dc1b1a617
Brandin E. Jefferson <brandinjefferson93@gmail.com>
2014-10-28 02:13:12 -0500

    Strings are symbols, need to fix case

--- metacircular.scm
+++ metacircular.scm
@@ -50,10 +50,9 @@
                     (append e2 (if (null? (cddr e1)) (helper (cadr e1) e2) (helper (cdr e1) e2))))
                 (append e2 '#f)))
         ((eqv? (car e1) 'case)
-         ;Requires debugging
+         ;In the test example, a cond is being returned on its own, look at that
              (helper (list 'let (list (list 'key (cadr e1)) (casesequences (cddr e1) '() 1)) (list 'cond (list (caselists (cddr e1) '() 1)))) '()))  
         ((eqv? (car e1) 'let)
-         ;On cond portion, the cdr of the cadr is (), so debug this
             (append e2 (cons (list 'lambda (getvar (cadr e1) '()) (helper (caddr e1) '())) (getinit (cadr e1) '()))))
         ((eqv? (car e1) 'let*)
             (if (null? (cadr e1))
@@ -87,13 +86,13 @@
 (define getinitrec (lambda (e1 e2 num)
                      (if (not (null? e1))
                          (if (pair? (car (cdar e1))) 
-                          (getinitrec (cdr e1) (append e2 (list `,(string-append "temp" (number->string num)) (cons (helper (list (car (cdar e1))) '()) '()))) (+ num 1))
-                          (getinitrec (cdr e1) (append e2 (list `,(string-append "temp" (number->string num)) (helper (list (car (cdar e1))) '()))) (+ num 1)))
+                          (getinitrec (cdr e1) (append e2 (list `,(string->symbol (string-append "temp" (number->string num))) (cons (helper (list (car (cdar e1))) '()) '()))) (+ num 1))
+                          (getinitrec (cdr e1) (append e2 (list `,(string->symbol (string-append "temp" (number->string num))) (helper (list (car (cdar e1))) '()))) (+ num 1)))
                       e2)))
 
 (define getcombos (lambda (e1 e2 num) 
                     (if (not (null? e1))
-                        (getcombos (cdr e1) (append e2 (list 'set! (caar e1) `,(string-append "temp" (number->string num)))) (+ num 1))
+                        (getcombos (cdr e1) (append e2 (list 'set! (caar e1) `,(string->symbol (string-append "temp" (number->string num))))) (+ num 1))
                         e2)))
 
 (define casesequences (lambda (e1 e2 num) 
@@ -101,14 +100,14 @@
                             e2
                             (if (eqv? (caar e1) 'else)
                                 (append e2 (list 'elsethunk (list 'lambda '() (helper (cdar e1) '()))))
-                                (casesequences (cdr e1) (append e2 (list (list `,(string-append "thunk" (number->string num))) (list 'lambda '() (helper (cdar e1) '())))) (+ num 1))))))
+                                (casesequences (cdr e1) (append e2 (list (list`,(string->symbol (string-append "thunk" (number->string num))) (list 'lambda '() (helper (cdar e1) '()))))) (+ num 1))))))
 
 (define caselists (lambda (e1 e2 num)
                     (if (null? e1)
                         e2
                         (if (eqv? (caar e1) 'else)
                             (append e2 (list (list (caar e1) (list 'elsethunk))))
-                            (caselists (cdr e1) (append e2 (list  (list 'memv 'key (caar e1)) (list (list `,(string-append "thunk" (number->string num)))))) (+ num 1)))))) 
+                            (caselists (cdr e1) (append e2 (list (list (list 'memv 'key (caar e1)) (list `,(string->symbol (string-append "thunk" (number->string num))))))) (+ num 1)))))) 
 
 (define test '(case (* 2 3)
   ((2 3 5 7) 'prime)
